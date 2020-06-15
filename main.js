454a3beb14be9d4251d6e53c18832c795788d3f7
jamesnitz <jamesanitz2@gmail.com>
2020-06-15 09:26:38 -0500

    playing with mapbox, and adding markers on ctrl + click

--- main.js
+++ main.js
@@ -1,8 +1,10 @@
 import {workOutListComponent} from "./workoutList.js"
 import {getWorkOuts} from "./workoutDataProvider.js"
-
+import {mappy} from "./mapbox.js"
 getWorkOuts().then(
   () => {
     workOutListComponent()
   }
 )
+
+mappy()
---
+++ mapbox.js
@@ -0,0 +1,39 @@
+export const mappy = () => {
+  const eventHub = document.querySelector(".container");
+  mapboxgl.accessToken = 'pk.eyJ1IjoiamFtZXNuaXR6IiwiYSI6ImNrYmdpdzFrdzE2MTcyb21rOW92dG9laWYifQ.shmWe90XzNgEIWPgdK2nVw';
+  var map = new mapboxgl.Map({
+  container: 'mapbox',
+  style: 'mapbox://styles/mapbox/streets-v11', // stylesheet location
+  center: [-86.78, 36.16], // starting position [lng, lat]
+  zoom: 9 // starting zoom
+  });
+
+new mapboxgl.Marker()
+  .setLngLat([-86.78, 36.16])
+  .addTo(map);
+
+  let keypressed = false;
+  eventHub.addEventListener("keydown", e => {
+    if (e.keyCode === 17 && keypressed === true) {
+      keypressed = false;
+      console.log("false")
+    }
+    else if (e.keyCode === 17 && keypressed === false) {
+      keypressed = true;
+      console.log("true")
+    }
+
+    
+    // Set up an event listener on the map.
+    map.on('click', event => {
+        if (keypressed === true) {
+        // The event object (e) contains information like the
+        // coordinates of the point on the map that was clicked.
+        console.log('A click event has occurred at ' + event.lngLat);
+        new mapboxgl.Marker()
+        .setLngLat(event.lngLat)
+        .addTo(map)
+      }
+      });
+    });
+  }
